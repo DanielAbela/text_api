8592638199a73915838493a33a938f0c0035203c
Daniel Abela <DanielAbela96@gmail.com>
2020-10-14 00:38:36 +0100

    Linting

--- text_api/populate_database.py
+++ text_api/populate_database.py
@@ -1,19 +1,17 @@
 import os
 
 import requests
-from flask import Flask
 
 from utils import scrape_url
 from .app import db, create_app
-
 
 LIST_VIEW_ENDPOINT = "http://127.0.0.1:5000/texts/"
 URLS = [
     "https://squirro.com/2020/05/29/how_synpulse_is_using_squirro_over_aws_to_provide_fs_firms_with_a_competitive_edge/",
     "https://squirro.com/2020/09/25/the-squirro-approach-to-training-and-certification/",
     "https://squirro.com/2020/05/28/squirro-partners-with-wipro-to-help-customers-become-intelligent-enterprises/",
-    # "https://squirro.com/2020/04/24/squirro-launches-transportation-monitoring-application/",
-    # "https://squirro.com/2020/03/27/a-letter-to-our-community-of-customer-prospects-and-partners/"
+    "https://squirro.com/2020/04/24/squirro-launches-transportation-monitoring-application/",
+    "https://squirro.com/2020/03/27/a-letter-to-our-community-of-customer-prospects-and-partners/"
 ]
 
 if os.path.exists("text.db"):
